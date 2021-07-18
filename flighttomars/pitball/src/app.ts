900b71a2df746b383ba4ead2c05938e3682a0cd1
Alex W <alex@metafaux.com>
2021-07-18 13:23:31 -0700

    flighttomars/pitball: create icosphere

--- flighttomars/pitball/src/app.ts
+++ flighttomars/pitball/src/app.ts
@@ -1,12 +1,34 @@
 // Copyright (c) Alexander D. Wilson. All rights reserved.
 // Licensed under the MIT License.
 
-import * as MRE from "@microsoft/mixed-reality-extension-sdk";
+import * as MRE from '@microsoft/mixed-reality-extension-sdk';
 
 export default class PitBall {
-    private sphere: MRE.Actor = null;
+  private ball: MRE.Actor = null;
+  private assets: MRE.AssetContainer;
 
-    constructor(private context: MRE.Context) {
-        console.log("started");
-    }
+  constructor(private context: MRE.Context) {
+    this.context.onStarted(() => this.started());
+  }
+
+  private async started() {
+    console.log('started');
+
+    this.assets = new MRE.AssetContainer(this.context);
+
+    const ballData = await this.assets.loadGltf('ball.glb', 'box');
+
+    this.ball = MRE.Actor.CreateFromPrefab(this.context, {
+      firstPrefabFrom: ballData,
+      actor: {
+        name: 'Ball for ball pit',
+        transform: {
+          local: {
+            position: { x: 0, y: -1, z: 0 },
+            scale: { x: 0.4, y: 0.4, z: 0.4 },
+          },
+        },
+      },
+    });
+  }
 }
